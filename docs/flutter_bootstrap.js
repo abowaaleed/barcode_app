a56f65499ebf8ba53912e56f93ef10123a7c6e0e
Saleh Alhoodi <abo.waaleed@gmail.com>
2026-07-30 19:59:17 +0300

    fix: set base-href to /barcode_app/ for GitHub Pages subdirectory

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3385270952" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1879575115" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
